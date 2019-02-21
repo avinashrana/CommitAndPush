2aaaafa6a00ee59751f7d1baae90ca92da4e98af
Avinash Rana <Avinashrana@INLaptop135-2.local>
2019-02-21 15:39:06 +0530

    First comment

---
+++ src/main/java/com/Eclipse/pom/Commit_Push/App.java
@@ -0,0 +1,13 @@
+package com.Eclipse.pom.Commit_Push;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
